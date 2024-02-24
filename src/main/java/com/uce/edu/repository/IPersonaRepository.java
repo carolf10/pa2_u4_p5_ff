23bb340f60d4c5d622e3368ea54a525e9cc3de34
EquipoUce <equipouce5@gmail.com>
2024-02-23 21:15:44 -0500

    Taller #38

---
+++ src/main/java/com/uce/edu/repository/IPersonaRepository.java
@@ -0,0 +1,13 @@
+package com.uce.edu.repository;
+
+import java.util.List;
+
+import com.uce.edu.repository.modelo.Persona;
+
+public interface IPersonaRepository {
+	public Persona seleccionarPorCedula (String cedula);
+	public void insertar (Persona persona);
+	public void actualizar (Persona persona);
+	public void eliminarPorCedula (String cedula);
+	public List<Persona> consultarTodos();
+}
